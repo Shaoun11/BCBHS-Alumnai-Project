77ecf6e1db40e1a6a00442101f75abd2763a2a13
Minhazul Abdin <hey_minhaz@Minhazuls-MacBook-Air.local>
2023-11-22 02:05:12 +0600

    search

--- src/Components/Navber/Navber.jsx
+++ src/Components/Navber/Navber.jsx
@@ -7,61 +7,74 @@ import { Authcontext } from '../../Authprovider/Authprovider';
 
 
 const Navber = () => {
-      const { user, logout } = useContext(Authcontext);
+  const { user, logout } = useContext(Authcontext);
 
-      const handelSingout = () => {
-        logout().then().catch();
-      };
-    const link=<>
-    <li><NavLink to={"/"}>Home</NavLink></li>
-   <li><NavLink to={"/event"}> Event </NavLink></li>
-    <li><NavLink to={"/About"}>About</NavLink></li>
-    <li><NavLink to={"/alumniDatabase"}>Alumni Database</NavLink></li>
-    
 
-  </>
-    return (
-      <div className="navbar sticky -top-2 z-20 bg-opacity-30 h-20 bg-white static:text-white  ">
-        <div className="navbar-start ">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+
+  const handelSingout = () => {
+    logout().then().catch();
+  };
+  const link = (
+    <>
+      <li>
+        <NavLink to={"/"}>Home</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/event"}> Event </NavLink>
+      </li>
+      <li>
+        <NavLink to={"/About"}>About</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/alumniDatabase"}>Alumni Database</NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar sticky -top-2 z-20 bg-opacity-30 h-20 bg-white static:text-white  ">
+      <div className="navbar-start ">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              {link}
-            </ul>
-          </div>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {link}
+          </ul>
+        </div>
 
-       <Link to='/'>   <img
+        <Link to="/">
+          {" "}
+          <img
             className="lg:w-28 md:w-32 w-20"
             src="https://i.ibb.co/0nShLXT/Community-Logo-1.png"
             alt=""
-          /></Link>
-          <a className="text-gray-600 normal-case font-bold mt-1 -ml-3 lg:text-sm md:text-sm text-[10px] ">
-            BCBHS Alumni Association
-          </a>
-        </div>
-        <div className="navbar-center  hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">{link}</ul>
-        </div>
-        <div className="lg:flex  md:flex gap-3 navbar-end">
-          {/* <p className=''>{user?.displayName}</p>
+          />
+        </Link>
+        <a className="text-gray-600 normal-case font-bold mt-1 -ml-3 lg:text-sm md:text-sm text-[10px] ">
+          BCBHS Alumni Association
+        </a>
+      </div>
+      <div className="navbar-center  hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{link}</ul>
+      </div>
+      <div className="lg:flex  md:flex gap-3 navbar-end">
+        {/* <p className=''>{user?.displayName}</p>
       <div>
       <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
           {user? <div className="w-14 rounded-full">
@@ -82,57 +95,56 @@ const Navber = () => {
 
     
      </div> */}
-          {user?.email ? (
-            <div className="dropdown dropdown-end " >
-              <label tabIndex={0} className="cursor-pointer">
-                <div className="flex gap-2">
-                  <div className="mt-2">
-                    <p>{user?.displayName}</p>
-                  </div>
-                  <div className="avatar">
-                    <div className="w-10 rounded-full">
-                      <img src={user?.photoURL} />
-                    </div>
-                  </div>
+        {user?.email ? (
+          <div className="dropdown dropdown-end ">
+            <label tabIndex={0} className="cursor-pointer">
+              <div className="flex gap-2">
+                <div className="mt-2">
+                  <p>{user?.displayName}</p>
                 </div>
-              </label>
-              <div
-                tabIndex={0}
-                className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-              >
-                <NavLink 
-                  to="/myprofile"
-                  className="px-4 py-2 hover:bg-base-300 rounded-lg"
-                >
-                  My Profile
-                </NavLink>
-               
-
-                <div
-                  onClick={handelSingout}
-                  className="cursor-pointer text-red-500 px-4 py-2 hover:bg-base-300 rounded-lg"
-                >
-                  Logout
+                <div className="avatar">
+                  <div className="w-10 rounded-full">
+                    <img src={user?.photoURL} />
+                  </div>
                 </div>
               </div>
-            </div>
-          ) : (
-            <div>
+            </label>
+            <div
+              tabIndex={0}
+              className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+            >
               <NavLink
-                to="/login"
-                className={({ isActive }) =>
-                  isActive
-                    ? "btn btn-outline cursor-pointer items-center justify-center rounded-md border py-2 px-8 text-center text-gray-500 transition duration-150 ease-in-out hover:translate-y-1 hover:bg-green-600 hover:text-white"
-                    : "btn btn-outline cursor-pointer items-center justify-center rounded-md border py-2 px-8 text-center text-gray-500 transition duration-150 ease-in-out hover:translate-y-1 hover:bg-yellow-400 hover:text-white"
-                }
+              
+                className="px-4 py-2 hover:bg-base-300 rounded-lg"
               >
-                Login
+                My Profile
               </NavLink>
+
+              <div
+                onClick={handelSingout}
+                className="cursor-pointer text-red-500 px-4 py-2 hover:bg-base-300 rounded-lg"
+              >
+                Logout
+              </div>
             </div>
-          )}
-        </div>
+          </div>
+        ) : (
+          <div>
+            <NavLink
+              to="/login"
+              className={({ isActive }) =>
+                isActive
+                  ? "btn btn-outline cursor-pointer items-center justify-center rounded-md border py-2 px-8 text-center text-gray-500 transition duration-150 ease-in-out hover:translate-y-1 hover:bg-green-600 hover:text-white"
+                  : "btn btn-outline cursor-pointer items-center justify-center rounded-md border py-2 px-8 text-center text-gray-500 transition duration-150 ease-in-out hover:translate-y-1 hover:bg-yellow-400 hover:text-white"
+              }
+            >
+              Login
+            </NavLink>
+          </div>
+        )}
       </div>
-    );
+    </div>
+  );
 };
 
 export default Navber;
--- src/Database/Database.jsx
+++ src/Database/Database.jsx
@@ -1,14 +1,23 @@
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable react/no-unescaped-entities */
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Navber from "../Components/Navber/Navber";
 import "./datacard.css"
 import DatabaseCard from "./DatabaseCard";
+import { FiSearch } from "react-icons/fi";
 
 
 
 const Database = () => {
     const [userdata,setuserdata]=useState([]);
-    const [loader,setloader]=useState(true)
+    const [loader, setloader] = useState(true);
+
+     
+      const [search, setsearch] = useState("");
+
+      const searchQuery = search.toLowerCase();
+
     useEffect(()=>{
         axios.get('http://localhost:5001/userData')
         .then((res)=>{
@@ -24,46 +33,73 @@ const Database = () => {
   
     console.log(userdata);
     return (
-        <>
+      <>
         <Navber></Navber>
+
         <div>
-            <h1 className="md:text-5xl text-2xl text-center font-semibold mt-5 mb-16 ">Our <span className="text-red-500">Alumni</span> Database</h1>
+          <div>
+            <h1 className="md:text-5xl text-2xl text-center font-semibold mt-5 mb-16 ">
+              Our <span className="text-red-500">Alumni</span> Database
+            </h1>
+            <div className=" flex justify-center mt-3 ">
+              <div className="sm:flex items-center bg-white rounded-lg overflow-hidden px-2 py-1 justify-between  w-1/2 shadow-lg ">
+                <input
+                  className="text-base text-gray-400 flex-grow outline-none px-2 py-3"
+                  onChange={(e) => setsearch(e.target.value)}
+                  type="text"
+                  name="search"
+                  placeholder="Search by name"
+                />
+                <div className="ms:flex items-center px-2 py-1  rounded-lg space-x-4 mx-auto">
+                  <FiSearch className="text-xl"></FiSearch>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* search */}
+
+          <div className="flex items-center justify-center">
+            <div className="container">
+              <table className="w-full flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5">
+                <thead className="text-white">
+                  <tr className="bg-red-400 flex md:visible hidden flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
+                    <th className="p-3 text-left">Name</th>
+                    <th className="p-3 text-left">Batch</th>
+                    <th className="p-3 text-left">Email</th>
+
+                    <th className="p-3 text-left" width="140px">
+                      Last Roll
+                    </th>
+                    <th className="p-3 text-left" width="140px">
+                      Profession
+                    </th>
+                    <th className="p-3 text-left" width="100px">
+                      Social
+                    </th>
+                    <th className="p-3 text-left" width="110px">
+                      Details
+                    </th>
+                  </tr>
+                </thead>
+              </table>
+            </div>
+          </div>
         </div>
-      <div>
-      <div className="flex items-center justify-center">
-	<div className="container">
-		<table className="w-full flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5">
-        <thead className="text-white">
-				<tr className="bg-red-400 flex md:visible hidden flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
-					<th className="p-3 text-left">Name</th>
-					<th className="p-3 text-left">Batch</th>
-					<th className="p-3 text-left">Email</th>
-					
-					<th className="p-3 text-left" width="140px">Last Roll</th>
-					<th className="p-3 text-left" width="140px">Profession</th>
-					<th className="p-3 text-left" width="140px">Social</th>
-					<th className="p-3 text-left" width="110px">Details</th>
-				</tr>
-				
-			</thead>
-                
-                </table>
-			</div>
-			</div>
-       
-      
-      </div>
-   <div>
-   {
-                userdata.map(user=><DatabaseCard key={user?._id} data={user} ></DatabaseCard>)
-            }
-   </div>
+        <div>
+          {userdata
+            .filter((item) => {
+              return item.name.toLowerCase().includes(searchQuery);
+            })
+            .map((user) => (
+              <DatabaseCard key={user?._id} data={user}></DatabaseCard>
+            ))}
+        </div>
 
-
-        </>
-  
-      
+        <div>
        
+        </div>
+      </>
     );
 };
 
